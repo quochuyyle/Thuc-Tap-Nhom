95048b23a1a38595bbdacf9fba7a22f85ed7ad39
quochuyyle <48556751+quochuyyle@users.noreply.github.com>
2019-05-18 22:48:52 +0700

    My first Commit

---
+++ DangNhap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace Quan_li_diem_HS_tieu_hoc
+{
+    public class DangNhap
+    {
+        public DataTable TaiKhoan(string TaiKhoan)
+        {
+            string strSQL = "Select TaiKhoan,MatKhau from TaiKhoan where TaiKhoan='"+TaiKhoan+"'";
+            return DataProvider.LayDanhSach(strSQL);
+        }
+
+        public DataTable DoiMK(string TaiKhoan,string MKmoi)
+        {
+            string strSQL = "Update TaiKhoan set MatKhau='"+MKmoi+"' where TaiKhoan='"+TaiKhoan+"'";
+            return DataProvider.LayDanhSach(strSQL);
+        }
+    }
+}
