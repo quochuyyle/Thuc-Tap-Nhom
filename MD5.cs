30bcad19395c45a3295ce05ea5412eec22d83905
Duc Pham <ducpham161298@gmail.com>
2019-05-22 15:06:34 +0700

    22-5

---
+++ MD5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quan_li_diem_HS_tieu_hoc
+{
+    public class MD5
+    {
+        public static String GetMD5(string txt)
+        {
+            String str= "";
+            Byte[] buffer = System.Text.Encoding.UTF8.GetBytes(txt);
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            buffer = md5.ComputeHash(buffer);
+            foreach(Byte b in buffer)
+            {
+                str +=b.ToString("x2");
+
+            }
+            return str;
+        }
+
+    }
+}
